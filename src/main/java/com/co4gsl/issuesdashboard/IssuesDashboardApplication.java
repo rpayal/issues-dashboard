b68d1a3595e29778f3e0a78a7d08f3fae367085e
RPAYAL <rpayal@hotmail.com>
2018-10-20 19:39:50 +0100

    Initial commit with project structure and H2 DB integration

---
+++ src/main/java/com/co4gsl/issuesdashboard/IssuesDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.co4gsl.issuesdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuesDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuesDashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/co4gsl/issuesdashboard/IssuesDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.co4gsl.issuesdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IssuesDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
